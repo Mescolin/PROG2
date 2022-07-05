fd1aa63b95c9db27f3a542fcc4d6650e7ee20d03
Daniel Mescolin <72096313+Mescolin@users.noreply.github.com>
2022-07-05 20:54:15 -0300

    Add files via upload

---
+++ empregado_arraylist/Empregado_Arraylist.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package empregado_arraylist;
+
+/**
+ *
+ * @author danie
+ */
+public class Empregado_Arraylist {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
